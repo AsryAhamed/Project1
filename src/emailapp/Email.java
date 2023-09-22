55ec0de5495ba07dd729ae04c96d60eca00f15e1
asus <asus@172.28.15.2>
2023-09-22 18:28:41 +0530

    2nd commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,26 @@
+package emailapp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	//Constructor to receive the first name and last name
+	public Email(String firstName , String lastNmae) {
+		this.firstName = firstName;
+		this.lastName = lastNmae;
+		System.out.println("EMAIL CREATED: " + this.firstName + " "+ this.lastName);
+	}
+	
+	// Ask for the department
+	
+	// Generate a random password
+	
+	// Set the mailbox capacity
+	
+	// Change the password
+
+}
