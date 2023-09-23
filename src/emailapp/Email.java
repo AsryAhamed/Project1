7b3f0e0745410962ea700b16ebf3fd068059ce3a
asus <asus@172.28.15.2>
2023-09-23 16:52:49 +0530

    commit 4

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
 	private String password;
 	private String department;
 	private int mailboxCapacity;
+	private int defaultPasswordLength;
 	private String alternateEmail;
 	
 	//Constructor to receive the first name and last name
@@ -18,11 +19,16 @@ public class Email {
 		
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
+		System.out.println("Deprtment: " + this.department);
+		
+		// call a method return a random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: "+ this.password);
 	}
 	
 	// Ask for the department
 	private String setDepartment() {
-		System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) {return "sales"; }
@@ -34,6 +40,15 @@ public class Email {
 	}
 	
 	// Generate a random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHTJKLMNOPQRSTUWXYZ0123456789!@#$%";
+		char []password = new char[length];
+		for (int i=0; i<length; i++) {
+			int rand = (int) (Math.random()* passwordSet.length());
+			password[i]= passwordSet.charAt(rand);
+		}
+		return new String(password);
+	}
 	
 	// Set the mailbox capacity
 	
