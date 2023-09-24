22d2aef2fb25ec4fb4e9851cad47a52b1212b6c4
asus <asus@172.28.15.2>
2023-09-24 10:09:40 +0530

    commit 5

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,9 +7,11 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
+	private String email;
 	private int mailboxCapacity;
 	private int defaultPasswordLength = 10;
 	private String alternateEmail;
+	private String companySuffix = "aeycompany.com";
 	
 	//Constructor to receive the first name and last name
 	public Email(String firstName , String lastNmae) {
@@ -24,6 +26,11 @@ public class Email {
 		// call a method return a random password
 		this.password = randomPassword(defaultPasswordLength);
 		System.out.println("Your password is: "+ this.password);
+		
+		// Combine elements to email
+		email =  firstName.toLowerCase()+ "." + lastName.toLowerCase()+ "@" + department + "." + companySuffix;
+		System.out.println("Your email is: " + email);
+		
 	}
 	
 	// Ask for the department
